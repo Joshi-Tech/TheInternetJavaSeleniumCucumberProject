ca4ad6dee1cab51dc11d235c38d9b86328a12108
laxmi kant joshi <lkjoshi.tech@gmail.com>
2025-01-02 23:56:56 +0000

    updated a failing test

--- src/main/java/pages/FramesPage.java
+++ src/main/java/pages/FramesPage.java
@@ -2,6 +2,11 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class FramesPage extends CommonAttributes {
 
@@ -10,7 +15,11 @@ public class FramesPage extends CommonAttributes {
     }
 
     public String iFrameText() {
+        //driver.findElement(By.cssSelector("div[aria-label='Close']")).click();
         driver.findElement(By.cssSelector("a[href='/iframe']")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".tox-edit-area__iframe")));
+
         driver.switchTo().frame(driver.findElement(By.cssSelector(".tox-edit-area__iframe")));
         return driver.findElement(By.cssSelector("#tinymce>p")).getText();
     }
