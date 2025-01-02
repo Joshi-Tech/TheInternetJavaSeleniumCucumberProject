ae00466ac32faa8e1f581619bf1e1b243de679ab
laxmi kant joshi <lkjoshi.tech@gmail.com>
2025-01-02 17:08:42 +0000

    Some files updated

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -1,19 +1,10 @@
 package pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage extends CommonAttributes {
 
     public HomePage(WebDriver driver) {
         super(driver);
-    }
-
-    public String getHeader() {
-        return driver.findElement(By.cssSelector(".heading")).getText();
-    }
-
-    public String getH2() {
-        return driver.findElement(By.cssSelector("#content>h2")).getText();
     }
 }
